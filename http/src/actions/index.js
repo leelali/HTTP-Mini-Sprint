87a504e7b082b7a38a3e80eb8f470ea7814f90b9
Xiaoping Li <narci2008@gmail.com>
2017-12-13 12:13:05 -0800

    resolution

--- http/src/actions/index.js
+++ http/src/actions/index.js
@@ -4,4 +4,18 @@ export const GET_FRIENDS = 'GET_FRIENDS';
 
 export const getFriends = () => {
     // Add the code for this action
+    const apiUrl = 'http://localhost:5000/friends';
+    const friendsRequest = axios.get(apiUrl);
+    // redux-promise middleware takes care of resolving the promise
+    return {
+    	type: GET_FRIENDS,
+    	payload: friendsRequest
+    };
 };
+
+
+
+// http is stateless
+// most operations are asynchronous
+// Promise: is an object, substitute for callbacks
+// 
